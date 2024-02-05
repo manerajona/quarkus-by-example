61c379edd208d0eb91de9db20def292c4276769c
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-02-05 15:11:01 +0100

    Implementing CategoriesResource as dummy-impl with JAX-RS-classic

---
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/api/CategoriesResource.java
@@ -0,0 +1,35 @@
+package de.schulte.smartbar.backoffice.api;
+
+import java.util.List;
+
+import de.schulte.smartbar.backoffice.api.model.Category;
+
+public class CategoriesResource implements CategoriesApi {
+
+    private final Category category = new Category().name("drinks");
+
+    @Override
+    public void categoriesCategoryIdDelete(String categoryId) {
+
+    }
+
+    @Override
+    public Category categoriesCategoryIdGet(String categoryId) {
+        return category;
+    }
+
+    @Override
+    public void categoriesCategoryIdPut(String categoryId, Category category) {
+
+    }
+
+    @Override
+    public List<Category> categoriesGet() {
+        return List.of(category);
+    }
+
+    @Override
+    public void categoriesPost(Category category) {
+
+    }
+}
--- smartbar-backoffice/src/test/java/de/schulte/smartbar/backoffice/api/CategoriesResourceIT.java
+++ smartbar-backoffice/src/test/java/de/schulte/smartbar/backoffice/api/CategoriesResourceIT.java
@@ -1,8 +1,8 @@
-package de.schulte;
+package de.schulte.smartbar.backoffice.api;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
+class CategoriesResourceIT extends CategoriesResourceTest {
     // Execute the same tests but in packaged mode.
 }
