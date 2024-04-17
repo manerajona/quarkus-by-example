73d2aa55292d045a036786c8f882607fab6a7c8b
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-04-17 22:03:10 +0200

    Skeleton for simple vue.js backend for smartbar-backoffice

---
+++ smartbar-backoffice-ui/src/components/Tables.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {OAuth2Client} from "@badgateway/oauth2-client";
+import axios from "axios";
+
+const tables = ref({});
+
+onMounted(() => {
+  new OAuth2Client({
+    server: '',
+    clientId: '...'
+  })
+
+  axios.get("/api/tables").then(response => (tables.data = response.data))
+})
+</script>
+
+<template>
+  <h1>Smartbar - Tables overview</h1>
+</template>
