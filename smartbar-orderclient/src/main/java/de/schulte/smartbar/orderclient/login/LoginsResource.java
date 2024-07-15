1763035a4f92fd45b5a99bc309fbd25fbd7ae2e1
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-07-15 21:47:33 +0200

    Fetching menu with Java HTTP client

---
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
@@ -0,0 +1,26 @@
+package de.schulte.smartbar.orderclient.login;
+
+import de.schulte.smartbar.orderclient.api.LoginsApi;
+import de.schulte.smartbar.orderclient.api.model.LoginResponseBody;
+import io.smallrye.common.annotation.NonBlocking;
+import jakarta.inject.Inject;
+
+@NonBlocking
+public class LoginsResource implements LoginsApi {
+
+    private final MenuApiClient menuApiClient;
+
+    private final MenuMapper menuMapper;
+
+    @Inject
+    public LoginsResource(MenuApiClient menuApiClient, MenuMapper menuMapper) {
+        this.menuApiClient = menuApiClient;
+        this.menuMapper = menuMapper;
+    }
+
+    @Override
+    public LoginResponseBody login(String tableId) {
+        return menuMapper.mapToLoginResonse(menuApiClient.getMenu());
+    }
+
+}
--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClientImpl.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClientImpl.java
@@ -1,8 +1,16 @@
 package de.schulte.smartbar.orderclient.login;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.schulte.smartbar.backoffice.api.model.ApiMenu;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @ApplicationScoped
 public class MenuApiClientImpl implements MenuApiClient {
@@ -15,6 +23,13 @@ public class MenuApiClientImpl implements MenuApiClient {
 
     @Override
     public ApiMenu getMenu() {
-        return null;
+        try {
+            final var request = HttpRequest.newBuilder(new URI(apiUrl)).GET().build();
+            final var httpClient = HttpClient.newBuilder().build();
+            final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return new ObjectMapper().readValue(response.body(), ApiMenu.class);
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
