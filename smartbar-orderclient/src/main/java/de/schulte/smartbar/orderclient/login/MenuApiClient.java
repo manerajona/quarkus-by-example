391baf6dc6074825748227f22e7157b69ffc24cb
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-07-19 16:12:01 +0200

    Fault tolerance Timeouts

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
@@ -4,6 +4,7 @@ import de.schulte.smartbar.backoffice.api.model.ApiMenu;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import java.util.concurrent.CompletionStage;
@@ -13,7 +14,8 @@ import java.util.concurrent.CompletionStage;
 public interface MenuApiClient {
 
     @GET
-    @Retry(delay = 6000)
+    @Timeout(30)
+    @Retry(maxRetries = 10)
     CompletionStage<ApiMenu> getMenu();
 
 }
