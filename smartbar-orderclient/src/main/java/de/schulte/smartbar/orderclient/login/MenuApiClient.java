3388042d66fa5cbfeebc5db3fcfb460855e17c79
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-07-19 16:25:12 +0200

    Fault tolerance Fallback

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
@@ -1,12 +1,15 @@
 package de.schulte.smartbar.orderclient.login;
 
 import de.schulte.smartbar.backoffice.api.model.ApiMenu;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 @Path("/menu")
@@ -14,8 +17,15 @@ import java.util.concurrent.CompletionStage;
 public interface MenuApiClient {
 
     @GET
-    @Timeout(30)
-    @Retry(maxRetries = 10)
+    @Timeout(15)
+    @Retry(maxRetries = 2)
+    @Fallback(fallbackMethod = "getFallbackMenu")
     CompletionStage<ApiMenu> getMenu();
+
+    default CompletionStage<ApiMenu> getFallbackMenu() {
+        final var fallback = new ApiMenu();
+        fallback.setCategories(List.of());
+        return Uni.createFrom().item(fallback).subscribeAsCompletionStage();
+    }
 
 }
