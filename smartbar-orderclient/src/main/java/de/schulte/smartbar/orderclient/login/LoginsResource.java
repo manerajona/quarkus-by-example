b9c91b92a252359a3bddc779ac8572d926b56f9a
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-09-20 14:34:50 +0200

    Cache invalidate by annotation

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
@@ -2,8 +2,10 @@ package de.schulte.smartbar.orderclient.login;
 
 import de.schulte.smartbar.orderclient.api.LoginsApi;
 import de.schulte.smartbar.orderclient.api.model.LoginResponseBody;
+import io.quarkus.cache.CacheInvalidateAll;
 import io.quarkus.cache.CacheResult;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.util.concurrent.CompletionStage;
@@ -25,5 +27,9 @@ public class LoginsResource implements LoginsApi {
     public CompletionStage<LoginResponseBody> login(String tableId) {
         return menuApiClient.getMenu().thenApply(menuMapper::mapToLoginResonse);
     }
+
+    @CacheInvalidateAll(cacheName = "menu-cache")
+    @DELETE
+    public void invalidateCacheByKey() {}
 
 }
