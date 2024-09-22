41c2c4d3f00dd289827cdc38a08222ff6d8a2c5b
Ansgar Schulte <info@ansgarschulte-itconsulting.de>
2024-09-22 20:50:18 +0200

    Programmatic access

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
@@ -2,7 +2,9 @@ package de.schulte.smartbar.orderclient.login;
 
 import de.schulte.smartbar.orderclient.api.LoginsApi;
 import de.schulte.smartbar.orderclient.api.model.LoginResponseBody;
-import io.quarkus.cache.CacheResult;
+import io.quarkus.cache.Cache;
+import io.quarkus.cache.CacheName;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -14,16 +16,20 @@ public class LoginsResource implements LoginsApi {
 
     private final MenuMapper menuMapper;
 
+    private final Cache menuCache;
+
     @Inject
-    public LoginsResource(@RestClient MenuApiClient menuApiClient, MenuMapper menuMapper) {
+    public LoginsResource(@RestClient MenuApiClient menuApiClient, MenuMapper menuMapper, @CacheName("menu-cache") Cache menuCache) {
         this.menuApiClient = menuApiClient;
         this.menuMapper = menuMapper;
+        this.menuCache = menuCache;
     }
 
     @Override
-    @CacheResult(cacheName = "menu-cache")
     public CompletionStage<LoginResponseBody> login(String tableId) {
-        return menuApiClient.getMenu().thenApply(menuMapper::mapToLoginResonse);
+        return menuCache.getAsync(tableId, id -> {
+            return Uni.createFrom().completionStage(menuApiClient.getMenu().thenApply(menuMapper::mapToLoginResonse));
+        }).subscribeAsCompletionStage();
     }
 
 }
