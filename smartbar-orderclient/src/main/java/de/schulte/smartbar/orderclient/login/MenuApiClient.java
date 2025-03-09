ab57bbf7d931c6a9c8ec25464193ceed16024ee2
Jonathan Manera <manerajona@gmail.com>
2025-03-09 10:52:35 -0300

    Added Rate Limit

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
@@ -2,6 +2,7 @@ package de.schulte.smartbar.orderclient.login;
 
 import de.schulte.smartbar.backoffice.api.model.ApiMenu;
 import io.quarkus.cache.CacheResult;
+import io.smallrye.faulttolerance.api.RateLimit;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -21,9 +22,10 @@ public interface MenuApiClient {
 
     @GET
     @Timeout(1000)
-    @Retry
+    @Retry(maxRetries = 4, delay = 6000)
     @Fallback(fallbackMethod = "getFallbackMenu")
-    @CircuitBreaker(delay = 1, delayUnit = ChronoUnit.MINUTES)
+    @CircuitBreaker(requestVolumeThreshold = 4, delay = 1, delayUnit = ChronoUnit.MINUTES)
+    @RateLimit(value = 50, window = 1)
     @CacheResult(cacheName = "menu-cache")
     CompletionStage<ApiMenu> getMenu();
 
